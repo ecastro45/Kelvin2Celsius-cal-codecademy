84790c161d531b06004b2b8f72b1485514f86b23
Edwin <edwincastro77@gmail.com>
2017-06-08 21:51:26 -0400

    javascript program to convert kelvin to celsius
    This program uses a bit of ES6 as well as simple variables and consle.log methods.
    One of the first projects they have you do.

---
+++ k2c.js
@@ -0,0 +1,11 @@
+// Constant variable for Kelvin
+const kelvin = prompt(`What is the Kelvins temperature today?`);
+
+// Changable variable for celsius
+let celsius = kelvin-273;
+
+// Calculates celsius to Fahrenheit
+let Fahrenheit = celsius * (9/5) + 32;
+
+// Console log that outputs string with temperature with rounded temp
+console.log(`The temperature is ${Fahrenheit = Math.floor(Fahrenheit)} degrees fahrenheit.`);
